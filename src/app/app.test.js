dd6f981c94cd84048559bcfa9acc6589484ee024
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2020-03-16 23:41:43 +0000

    README. and test

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -7,7 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
-
-it('should Return Hello world', function () {
-  expect(HelloWorld()).toEqual('Hello World')
-})
